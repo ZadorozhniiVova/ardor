2f41acaa8cf499e9cf080f33084e173629f970d5
ZadorozhniiVova <zadorozhniiv@gmail.com>
2022-08-16 23:36:04 +0300

    update

---
+++ assets/css/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.content__slider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 1000,
+        infinite: true,
+        slidesToScroll: 1,
+        fade: true,
+        cssEase: 'linear',
+        focusOnSelect: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        
+    });
+    
+  });
